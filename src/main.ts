b3fc3a561108e573777ce4a7e6f84c44f7e1ef8f
lucasatdriano <lucasatdriano@gmail.com>
2025-07-05 20:07:38 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -41,6 +41,7 @@ async function bootstrap() {
     }),
   );
   await app.listen(port);
+  console.log(app.getHttpServer()._events.request._router.stack);
   logger.log(`Application running on port ${port}`);
 }
 
