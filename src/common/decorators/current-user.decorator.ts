1163aae3de2f6d7c592a38983bdca1ce4aa82888
lucasatdriano <lucasatdriano@gmail.com>
2025-07-05 04:07:12 -0300

    feat: add folder @types and add type in UserFromRequest

--- src/common/decorators/current-user.decorator.ts
+++ src/common/decorators/current-user.decorator.ts
@@ -4,22 +4,21 @@ import {
   SetMetadata,
 } from '@nestjs/common';
 import { Request } from 'express';
-import { UserFromRequest } from 'src/types/express';
 
-export const CurrentUser = createParamDecorator<
-  keyof UserFromRequest | undefined
->((field, ctx: ExecutionContext): unknown => {
-  const request = ctx.switchToHttp().getRequest<Request>();
-  const user = request.user as UserFromRequest | undefined;
+export const CurrentUser = createParamDecorator<string | undefined>(
+  (field, ctx: ExecutionContext): unknown => {
+    const request = ctx.switchToHttp().getRequest<Request>();
+    const user = request.user;
 
-  if (!user) {
-    throw new Error(
-      'CurrentUser decorator used without authentication. Did you add the AuthGuard?',
-    );
-  }
+    if (!user) {
+      throw new Error(
+        'CurrentUser decorator used without authentication. Did you add the AuthGuard?',
+      );
+    }
 
-  return field ? user[field] : user;
-});
+    return field ? user[field] : user;
+  },
+);
 
 export const REQUIRE_AUTH_KEY = 'requireAuth';
-export const RequireAuth = () => SetMetadata('requireAuth', true);
+export const RequireAuth = () => SetMetadata(REQUIRE_AUTH_KEY, true);
