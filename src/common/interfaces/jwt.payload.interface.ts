25e8e3a57087652aeda5473c2bfcda3a0047993b
lucasatdriano <lucasatdriano@gmail.com>
2025-07-05 04:21:30 -0300

    fix: email optional

--- src/common/interfaces/jwt.payload.interface.ts
+++ src/common/interfaces/jwt.payload.interface.ts
@@ -2,7 +2,7 @@ import { CameraType, UserRole } from '@prisma/client';
 
 export interface JwtPayload {
   sub: string;
-  email?: string;
+  email: string;
   role: UserRole;
   cameraType: CameraType | null;
   isActive: boolean;
