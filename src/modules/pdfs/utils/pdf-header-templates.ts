fdc2a8bf1402ddc55bcbe12d98099e662e5c2c74
lucasatdriano <lucasatdriano@gmail.com>
2025-09-06 01:31:53 -0300

    fix: table two in PDF Anexo M4

--- src/modules/pdfs/utils/pdf-header-templates.ts
+++ src/modules/pdfs/utils/pdf-header-templates.ts
@@ -18,28 +18,37 @@ export const getHeaderTemplate = (
           <div style="text-align: center; flex: 1; margin: 0 auto; position: absolute; left: 0; right: 0;">
             <h2 style="margin: 0; font-size: 12pt; font-weight: bold;">ANEXO M.3</h2>
           </div>
-          <div style="display: grid; grid-template-columns: repeat(3, 1fr); grid-template-rows: repeat(4, 15px); align-items: end; width: 100px; padding-right: 150px; font-size: 5pt;">
-              <p>Pág.:</p>
-              <hr style="width: 100%; border: .5px solid #3d3d3d; margin-bottom: 7px; grid-column: span 2;" />
-              <p>Rúblicas:</p>
-              <p style="margin-bottom: 7px; grid-column: span 2;"></p>
-              <p style="white-space: nowrap;">Resp. Téc.:</p>
-              <hr style="width: 100%; border: .5px solid #3d3d3d; margin-bottom: 7px; grid-column: span 2;" />
-              <p>CBMSP:</p>
-              <hr style="width: 100%; border: .5px solid #3d3d3d; margin-bottom: 7px; grid-column: span 2;" />
+          <div style="display: grid; grid-template-columns: repeat(3, 1fr); grid-template-rows: repeat(4, 12px); border: .5px solid #3d3d3d; padding: 2px; align-items: end; width: 100px; margin-right: 150px; padding-right: 5px; font-size: 4pt;">
+            <p>Pág.:</p>
+            <hr style="width: 100%; border: .5px solid #3d3d3d; margin-bottom: 5px; grid-column: span 2;" />
+            <p>Rúblicas:</p>
+            <p style="margin-bottom: 7px; grid-column: span 2;"></p>
+            <p style="white-space: nowrap;">Resp. Téc.:</p>
+            <hr style="width: 100%; border: .5px solid #3d3d3d; margin-bottom: 7px; grid-column: span 2;" />
+            <p>CBMSP:</p>
+            <hr style="width: 100%; border: .5px solid #3d3d3d; margin-bottom: 7px; grid-column: span 2;" />
           </div>
         </div>
       `;
 
     case 'anexo_m4':
       return `
-        <div style="display: flex; align-items: center; width: 100%; padding: 10px 2cm;">
-          ${baseLogo}
-          <div style="text-align: center; flex: 1; margin-left: 0px;">
-            <h2 style="margin: 0; font-size: 12pt; font-weight: bold;">ANEXO M.4</h2>
+          <div style="display: flex; align-items: center; width: 100%; padding: 10px 2cm;">
+            ${baseLogo}
+            <div style="text-align: center; flex: 1; margin: 0 auto; position: absolute; left: 0; right: 0;">
+              <h2 style="margin: 0; font-size: 12pt; font-weight: bold;">ANEXO M.4</h2>
+            </div>
+            <div style="display: grid; grid-template-columns: repeat(3, 1fr); grid-template-rows: repeat(4, 12px); border: .5px solid #3d3d3d; padding: 2px; align-items: end; width: 100px; margin-right: 150px; padding-right: 5px; font-size: 4pt;">
+              <p>Pág.:</p>
+              <hr style="width: 100%; border: .5px solid #3d3d3d; margin-bottom: 5px; grid-column: span 2;" />
+              <p>Rúblicas:</p>
+              <p style="margin-bottom: 7px; grid-column: span 2;"></p>
+              <p style="white-space: nowrap;">Resp. Téc.:</p>
+              <hr style="width: 100%; border: .5px solid #3d3d3d; margin-bottom: 7px; grid-column: span 2;" />
+              <p>CBMSP:</p>
+              <hr style="width: 100%; border: .5px solid #3d3d3d; margin-bottom: 7px; grid-column: span 2;" />
+            </div>
           </div>
-          <div style="width: 50px;"></div>
-        </div>
       `;
 
     case 'default':
